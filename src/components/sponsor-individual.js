d230974aa3dc3cf9567e12fc392caf4da6892c03
James Walker <jamesmawalker@gmail.com>
2021-03-08 21:37:35 +0700

    Create and add SVG for sponsor logos; still need to create wordmarks for visit button. Change svg size and fill color for expanded view. Add click to expand functionality for individual sponsor components. Remove logoMatrix, pass from sponsor info object in parent component. Refactor hover functionality to work in tandem with expand functionality. Still need to work out how to use markdown for blurbs.

--- src/components/sponsor-individual.js
+++ src/components/sponsor-individual.js
@@ -1,38 +1,13 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useStaticQuery, graphql } from "gatsby"
+import gsap from "gsap"
 
 import Img from "gatsby-image"
 
 import { AmtberLogo, AmtberWordmark } from '../components/Logo'
 
-const logoMatrix = {
-  amtber: {
-    logo: AmtberLogo,
-    wordmark: AmtberWordmark,
-  },
-  bikeLegal: {
-    logo: AmtberLogo,
-    wordmark: AmtberWordmark,
-  },
-  FC: {
-    logo: AmtberLogo,
-    wordmark: AmtberWordmark,
-  },
-  FS: {
-    logo: AmtberLogo,
-    wordmark: AmtberWordmark,
-  },
-  POC: {
-    logo: AmtberLogo,
-    wordmark: AmtberWordmark,
-  },
-  s4g: {
-    logo: AmtberLogo,
-    wordmark: AmtberWordmark,
-  },
-}
 
-const Sponsor = ({ id, blurb }) => {
+const Sponsor = ({ id, blurb, logo, active, expandSponsor, idx }) => {
   const data = useStaticQuery(graphql`
     query {
       amtber: file(relativePath: { eq: "sponsor-images/amtber.webp" }) {
@@ -80,37 +55,107 @@ const Sponsor = ({ id, blurb }) => {
     }
   `)
 
+
+  useEffect(() => {
+    gsap.fromTo(".blurb-area", 1, 
+      {
+        opacity: 0,
+        ease: "power2.inOut"
+      },
+      {
+        opacity: 1,
+        ease: "power2.inOut",
+        delay: .5,
+      },
+    )
+  }, [active])
+
   const [hovered, setHovered] = useState(false)
 
-  const hoverDebounce = (bool) => {
-    setTimeout(() => {
-      setHovered(bool)
-    }, 0)
+  const handleClick = () => {
+    expandSponsor(idx);
+    setHovered(false);
+  }
+
+  const handleVisitClick = (e) => {
+    e.stopPropagation()
   }
 
   return (
     <div
-      className={`sponsor ${hovered ? "hovered" : ""}`}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
+      className={`
+        sponsor 
+        ${hovered ? "hovered" : ""}
+        ${active ? "active" : ""}
+      `}
+      onClick={handleClick}
     >
-      <div
-        className="shade"
-      >
-        <div className="shade-logo">
-          <AmtberWordmark />
-        </div>
-      </div>
-      <div className="image-wrapper">
-        <Img
-          fadeIn
-          loading="eager"
-          fluid={data[id].childImageSharp.fluid}
-          objectFit="cover"
-          objectPosition="50% 100%"
-          alt={`Logo for the ${id} sponsor company`}
-        />
-      </div>
+      {!active ? (
+        <>
+          <div className="shade">
+            <div className="shade-logo">
+              {logo()}
+            </div>
+          </div>
+          <div className="image-wrapper">
+            <Img
+              fadeIn
+              loading="eager"
+              fluid={data[id].childImageSharp.fluid}
+              objectFit="cover"
+              objectPosition="50% 100%"
+              alt={`Logo for the ${id} sponsor company`}
+            />
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="shade-exp">
+            <div className="shade-exp-logo">
+              {logo()}
+            </div>
+          </div>
+          <div className="blurb-area">
+            <p className="blurb-text">
+              {/* {blurb} */}
+              We met John Amtber racing at Sea Otter in 2019. A recent vegan
+              himself, he saw us browsing through the vendor booths and couldn’t
+              believe his eyes when he saw) there was a vegan cycling team. He
+              immediately offered to service and lube our bikes with his
+              product, all the while regailing us with his story of going vegan
+              at 50 years old, and how he hadn’t felt so good since he was 25.
+              <br />
+              <br />
+              John is a man who does everything to the fullest - that’s true of
+              both his dedication to veganism, and of the consideration he puts
+              into his product. A chemical engineer by trade, he turned his
+              knowledge towards the many and varied problems of chain
+              lubricants. His line of aMTBer wax based lubricants have been
+              keeping our collective drivetrains buttery smooth and silent since
+              we met him.
+              <br />
+              <br />
+              We are proud to represent both a great guy, and a great product
+              with aMTBer.
+            </p>
+            <div className="visit-button-wrapper">
+              <button onClick={handleVisitClick} className="visit-button">
+                <p>VISIT</p> &nbsp; <span>|</span> &nbsp; {logo()}
+              </button>
+            </div>
+          </div>
+          <div className="image-wrapper exp">
+            <Img
+              fadeIn
+              loading="eager"
+              fluid={data[id].childImageSharp.fluid}
+              objectFit="cover"
+              objectPosition="50% 100%"
+              alt={`Logo for the ${id} sponsor company`}
+            />
+          </div>
+        </>
+      )}
     </div>
   )
 }
--- src/pages/sponsors.js
+++ src/pages/sponsors.js
@@ -1,64 +1,85 @@
-import React, { useEffect, useState } from 'react'
-import gsap from 'gsap'
+import React, { useEffect, useState } from "react"
+import gsap from "gsap"
 
-import Sponsor from '../components/sponsor-individual'
+import Sponsor from "../components/sponsor-individual"
 
-import { AmtberLogo, AmtberWordmark } from '../components/Logo'
+import {
+  AmtberLogo,
+  AmtberWordmark,
+  BikeLegalLogo,
+  FCLogo,
+  FSLogo,
+  POCLogo,
+  S4GLogo,
+} from "../components/Logo"
 
-import { SPONSOR_INFOS } from '../assets/partner-blurbs'
+import { SPONSOR_INFOS } from "../assets/partner-blurbs"
 
 const sponsorData = [
   {
     id: "amtber",
-    blurb: SPONSOR_INFOS.amtber
+    blurb: SPONSOR_INFOS.amtber,
+    logo: AmtberWordmark,
   },
   {
     id: "bikeLegal",
-    blurb: SPONSOR_INFOS.amtber
+    blurb: SPONSOR_INFOS.amtber,
+    logo: BikeLegalLogo,
   },
   {
     id: "FC",
-    blurb: SPONSOR_INFOS.amtber
+    blurb: SPONSOR_INFOS.amtber,
+    logo: FCLogo,
   },
   {
     id: "FS",
-    blurb: SPONSOR_INFOS.amtber
+    blurb: SPONSOR_INFOS.amtber,
+    logo: FSLogo,
   },
   {
     id: "POC",
-    blurb: SPONSOR_INFOS.amtber
+    blurb: SPONSOR_INFOS.amtber,
+    logo: POCLogo,
   },
   {
     id: "s4g",
-    blurb: SPONSOR_INFOS.amtber
+    blurb: SPONSOR_INFOS.amtber,
+    logo: S4GLogo,
   },
 ]
 
 const Sponsors = () => {
-  const [activeSponsor, setactiveSponsor] = useState(null)
+  const [hoveredSponsor, sethoveredSponsor] = useState(null)
+  const [activeSponsor, setActiveSponsor] = useState(null)
 
   useEffect(() => {
     gsap.from(".animation-wrapper", 1, {
-      opacity: 0
+      opacity: 0,
     })
-  }, [activeSponsor])
+  }, [hoveredSponsor])
+
+  const handleSponsorClick = idx => {
+    idx === activeSponsor ? setActiveSponsor(-1) : setActiveSponsor(idx)
+  }
 
   return (
     <section className="sponsors-container">
       <div className="absolute-wrapper">
         <div className="flex-wrapper">
-          {
-            sponsorData.map((s, idx) => {
-              const active = idx === activeSponsor ? "active" : ""
+          {sponsorData.map((s, idx) => {
+            const active = idx === activeSponsor ? "active" : ""
 
-              return (
-                <Sponsor
-                  id={s.id}
-                  blurb={s.blurb}
-                />
-              )
-            })
-          }
+            return (
+              <Sponsor
+                id={s.id}
+                idx={idx}
+                blurb={s.blurb.bio}
+                logo={s.logo}
+                active={active}
+                expandSponsor={handleSponsorClick}
+              />
+            )
+          })}
         </div>
       </div>
     </section>
